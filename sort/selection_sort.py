c18ca2d31a6674a8ad9f053a3f2b8a8a823d197e
minhtri1612 <10422119@vgu.edu.vn>
2025-11-10 21:36:15 +0700

    selection sort

---
+++ sort/selection_sort.py
@@ -0,0 +1,13 @@
+def selection_sort(arr):
+    for i in range(len(arr)-1):
+        min_index=i
+        for j in range(i+1,len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index=j
+        if i != min_index:
+            temp=arr[i]
+            arr[i]=arr[min_index]
+            arr[min_index]=temp
+    return arr
+
+print(selection_sort([4,2,6,5,1,3]))
