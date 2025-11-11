e0f87e823602c607d64a8ac5a7f707b820bdc593
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-11 09:53:25 +0700

    insertion sort

---
+++ sort/insertion_sort.py
@@ -0,0 +1,10 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        temp=arr[i]
+        j=i-1
+        while temp<arr[j] and j>-1:
+            arr[j+1]=arr[j]
+            arr[j]=temp
+            j-=1
+    return arr
+print(insertion_sort([4,2,6,5,1,3]))
