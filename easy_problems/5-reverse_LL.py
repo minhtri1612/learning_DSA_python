6e50642ced71ff4d79f9d946dbf792c7524451ff
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-12 20:46:03 +0700

    bai 5-reverse LL

---
+++ easy_problems/5-reverse_LL.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/reverse-linked-list/
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def reverseList(self, head):
+        prev = None
+        current = head
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        return prev
+    
+if __name__ == "__main__":
+    # Example usage:
+    node1 = ListNode(1)
+    node2 = ListNode(2)
+    node3 = ListNode(3)
+    node4 = ListNode(4)
+    node5 = ListNode(5)
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node5
+
+    reversed_head = Solution().reverseList(node1)
+    # Print reversed list
+    current = reversed_head
+    while current:
+        print(current.val)  # Output: 5 4 3 2 1
+        current = current.next
