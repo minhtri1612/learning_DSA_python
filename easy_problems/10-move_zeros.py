b748144563eb7aa98b43632d20803e1ca84c1fb6
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-13 10:07:39 +0700

    bai 10-move zeros

---
+++ easy_problems/10-move_zeros.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        j = 0 
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i] , nums[j] = nums[j], nums[i]
+                j+=1
+        return nums
+
+if __name__ == "__main__":
+    print(Solution().moveZeroes([0,1,0,3,12]))
+    print(Solution().moveZeroes([0]))
