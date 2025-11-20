c39da1a7840d806a5846307d8f44c9ecedf0d226
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-20 14:36:44 +0700

    forget to add 10 easy

--- practice/easy_problems/10-move_zeros.py
+++ practice/easy_problems/10-move_zeros.py
@@ -1,7 +1,7 @@
-class Solution(object):
-    def moveZeroes(self, nums):
+# class Solution(object):
+#     def moveZeroes(self, nums):
 
 
-if __name__ == "__main__":
-    print(Solution().moveZeroes([0,1,0,3,12]))
-    print(Solution().moveZeroes([0]))
+# if __name__ == "__main__":
+#     print(Solution().moveZeroes([0,1,0,3,12]))
+#     print(Solution().moveZeroes([0]))
