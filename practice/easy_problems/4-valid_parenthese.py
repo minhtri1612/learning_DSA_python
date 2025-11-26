1dcb95dead66d6706a9ea9d097b6729c8eb919de
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-26 21:43:41 +0700

    bai 4-valid_parentheses

---
+++ practice/easy_problems/4-valid_parenthese.py
@@ -0,0 +1,12 @@
+# # https://leetcode.com/problems/valid-parentheses/description/
+
+# class Solution(object):
+#     def isValid(self, s):
+
+
+# if __name__ == "__main__":
+#     print(Solution().isValid("()"))
+#     print(Solution().isValid("()[]{}"))
+#     print(Solution().isValid("(]"))
+#     print(Solution().isValid("([)]"))
+#     print(Solution().isValid("{[]}"))
