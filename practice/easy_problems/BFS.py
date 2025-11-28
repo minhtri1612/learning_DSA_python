400c25352060769c4c96a650771942dd705bb5ce
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-28 12:17:04 +0700

    btap BFS

---
+++ practice/easy_problems/BFS.py
@@ -0,0 +1,25 @@
+# def BFS(root):
+
+# # Example usage:
+# class TreeNode:
+#     def __init__(self, value):
+#         self.value = value
+#         self.left = None
+#         self.right = None
+# if __name__ == "__main__":
+#     # Creating a sample binary tree:
+#     #         1
+#     #        / \
+#     #       2   3
+#     #      / \
+#     #     4   5
+
+#     root = TreeNode(1)
+#     root.left = TreeNode(2)
+#     root.right = TreeNode(7)
+#     root.left.left = TreeNode(3)
+#     root.left.right = TreeNode(6)
+#     root.right.left = TreeNode(4)
+#     root.right.right = TreeNode(5)
+
+#     BFS(root)
